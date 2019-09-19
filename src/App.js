fa5a0b16771ad975470621ed6e0d0b1cd1e66d72
Axel Demborg <axel.demborg@gmail.com>
2019-09-19 16:03:24 +0200

    tidy upp and publish

--- src/App.js
+++ src/App.js
@@ -8,6 +8,11 @@ const cppnlink = "https://en.wikipedia.org/wiki/Compositional_pattern-producing_
 const channels = 4
 
 class CPPNCanvas extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { model : null}
+  }
+
   createModel(input_size) {
     const model = sequential();
     model.add(layers.dense({inputShape: [input_size], units: 8, activation: 'tanh', kernelInitializer: initializers.randomNormal({stddev: 1})}));
@@ -19,35 +24,32 @@ class CPPNCanvas extends React.Component {
     return model
   }
 
-  createInput(width, height, latent) {
+  createInput(width, height, latent, scale) {
     var a = []
     for (var y = 0; y < height; y++) {
-      const v = 10 * ((2*y/height)-1)
+      const v = scale * ((2*y/height)-1)
       for (var x = 0; x < width; x++) {
-        const u = 10 * ((2*x/width)-1)
-        a.push([u, v, Math.sqrt(u*u + v*v), ...latent])
+        const u = scale * ((2*x/width)-1)
+        a.push([u, v, Math.sqrt(u*u + v*v), ...latent.map(a => scale*a)])
       }
     }
     return tensor2d(a)
   }
 
   componentDidMount() {
-    const {width, height} = this.props
+    const {num_layers, hidden, width, height, scale} = this.props
+
+    
+    console.log('creating model')
+    const model = this.createModel(2+3, num_layers, hidden)
+    this.setState({model: model})
     const canvas = this.refs.canvas
     const ctx = canvas.getContext("2d")
     var imageData = ctx.createImageData(width, height)
-
-    const latent = Array.from({length: 2}, () => (2*Math.random()) -1);
-    const input = this.createInput(width, height, latent)
-    console.log("data dim: " + input.shape[1])
-    
-    console.log('creating model')
-    const model = this.createModel(input.shape[1])
+    const input = this.createInput(width, height, [1, -1], scale)
 
     const res = model.predict(input)
     const res_array = res.arraySync()
-
-    console.log("min: " + res.min().arraySync() + " max:" + res.max().arraySync())
 
     for (var i = 0; i < res_array.length; i++) {
       for (var c = 0; c < channels; c++) {
@@ -62,9 +64,10 @@ class CPPNCanvas extends React.Component {
     return (
       <div>
         <canvas 
-        ref="canvas" 
+        ref="canvas"
         width={this.props.width} 
-        height={this.props.height} />
+        height={this.props.height}
+        />
       </div>
     );
   }
@@ -77,7 +80,7 @@ function App() {
       <p>
         Playing with <a href={cppnlink}> CPPNs </a> and <a href={tflink}>tf.js</a> during hackathon
       </p>
-      <CPPNCanvas width={256} height={256}/>
+      <CPPNCanvas width={256} height={256} scale={8}/>
     </div>
   )  
 }
