0780ca00a6896960d005c6009be5fbbe67d1a129
Axel Demborg <axel.demborg@gmail.com>
2019-09-19 09:55:24 +0200

    make random noise image

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const tflink = "https://www.tensorflow.org/js"
+const cppnlink = "https://en.wikipedia.org/wiki/Compositional_pattern-producing_network"
+
+class CPPNCanvas extends React.Component {
+  componentDidMount() {
+    const canvas = this.refs.canvas
+    const ctx = canvas.getContext("2d")
+    var imageData = ctx.createImageData(100, 100)
+
+    for (var i = 0; i < imageData.data.length; i++) {
+      imageData.data[i] = Math.round(255 * Math.random());
+    }
+  
+    ctx.putImageData(imageData, 0, 0);
+  }
+
+  render() {
+    return (
+      <div>
+        <canvas ref="canvas" width={100} height={100} />
+      </div>
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>CPPN.js</h1>
+      <p>
+        Playing with <a href={cppnlink}> CPPNs </a> and <a href={tflink}>tf.js</a> during hackathon
+      </p>
+      <CPPNCanvas/>
     </div>
-  );
+  )  
 }
 
 export default App;
