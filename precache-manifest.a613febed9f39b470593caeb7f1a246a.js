78d16808ab81311532cdfedb6cff7bd94c3fbe2f
Axel Demborg <axel.demborg@gmail.com>
2019-09-19 16:15:55 +0200

    Updates

---
+++ precache-manifest.a613febed9f39b470593caeb7f1a246a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f4b9e14520b3e4a905ca6d174820233",
+    "url": "/cppn/index.html"
+  },
+  {
+    "revision": "12a88ce6d8b3d9d2fa78",
+    "url": "/cppn/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "fd2a31dde43e1ca35270",
+    "url": "/cppn/static/js/2.2ea7dc2c.chunk.js"
+  },
+  {
+    "revision": "12a88ce6d8b3d9d2fa78",
+    "url": "/cppn/static/js/main.3c5bf702.chunk.js"
+  },
+  {
+    "revision": "a91d9d63705ae53ff685",
+    "url": "/cppn/static/js/runtime~main.2977df63.js"
+  }
+]);
